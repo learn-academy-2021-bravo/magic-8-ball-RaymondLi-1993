a511297556852e80226185df3ff1c0ef944e34ee
Raymond Li <rlii0303@gmail.com>
2021-05-13 17:06:23 -0700

    added 8 ball logic

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      // ACTION ITEM: Add possible Magic8 Ball answers to the array
-      answerArray: ["Here is an answer"],
+
+      answerArray: ["I am not sure!", "Please Ask Again!", "Yes", "No", "All signs point to yes"],
       selectedAnswer: ""
     }
   }
 
   handleSubmit = () => {
     const { answerArray } = this.state
-    // ACTION ITEM: Your code here!!
-    // ACTION ITEM: Update the answerArray selection
-    this.setState({ selectedAnswer: answerArray[0] })
+    let random = Math.floor(Math.random() * 4);
+    this.setState({ selectedAnswer: answerArray[random]})
   }
+
 
   render(){
     return (
@@ -25,6 +25,7 @@ class App extends Component {
         <input
           className="inputBox"
           type="text"
+          placeholder="please ask a question"
         />
         <br />
         <button onClick={ this.handleSubmit }>
